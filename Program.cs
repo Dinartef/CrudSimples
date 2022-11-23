9b74917e79a958124187988c8fe028fcf63b179d
Dinartef <69481417+Dinartef@users.noreply.github.com>
2022-11-23 18:27:16 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ public static class Program
 {
     public static void Main(string [] args)
     {
+        Conta c = new ContaPoupanca();
+        c.saldo = 1000;
+        c.Sacar(200);
+        c.Depositar(10);
 
+        Conta c1 = new ContaCorrente();
+        c1.Saldo = 1000;
+        c1.Sacar(200);
+        c1.Depositar(10);
     }
 
 }
